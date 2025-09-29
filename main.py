f9c1593b0e1a28417858d373810110d46a75b75c
syndra <syndra@home.me>
2025-09-29 13:26:21 +0200

    github ready Readme

--- main.py
+++ main.py
@@ -3,30 +3,59 @@ import time
 import sys
 import itertools
 
+key_func="wtype"
+#key_func="xdotool"
+
 hw_midi="hw:1,0,1"
+
 color={"lighter_blue":"03","light_red":"04","light_green":"10","turquiose":"20","light_blue":"24","neon_blue":"29","dunno":"30","pink":"34","pinker":"38","dark_blue":"43","red":"48","green":"56","whiteish":"69"}
 led_modes={"pulsing":"9a","blinking":"9e","low_light":"93","default":"96"}
 
 apc_array=["20","21","22","23","24","25","26","27","18","19","1a","1b","1c","1d","1e","1f","10","11","12","13","14","15","16","17","08","09","0a","0b","0c","0d","0e","0f","00","01","02","03","04","05","06","07"]
 
+############################################################################################################################# 8x5
+# CHANGE HERE
 color_array=["dark_blue","light_blue","light_blue","light_blue","lighter_blue","lighter_blue","lighter_blue","pinker"]
 color_array=color_array+["dark_blue","light_blue","light_blue","light_blue","lighter_blue","lighter_blue","lighter_blue","pinker"]
 color_array=color_array+["dark_blue","light_blue","light_blue","light_blue","lighter_blue","lighter_blue","lighter_blue","pinker"]
 color_array=color_array+["dark_blue","light_blue","light_blue","light_blue","lighter_blue","lighter_blue","lighter_blue","pinker"]
 color_array=color_array+["light_green","red","light_blue","light_blue","lighter_blue","lighter_blue","lighter_blue","pinker"]
+#############################################################################################################################
+# CHANGE HERE
+func_conf=["","","","","","","",""]
+func_conf=func_conf+["","","","","","","",""]
+func_conf=func_conf+["","","","","","","",""]
+func_conf=func_conf+["","","","","","","",""]
+func_conf=func_conf+["w","q","n","","","","",""]
+###########################################################################################################################
+
+# UI BUTTONS
+#################################################### red bottom and vertical green
+ui_but=["40","41","42","43","44","45","46","47"]#——
+ui_but=ui_but+["52","53","54","55","56","51"]# |
+ui_but=ui_but+["5b","5d"]#play rec
+
+#################################################
+# CHANGE HERE
+ui_func=["","","","","","","",""]#——Left>Right
+ui_func=ui_func+["","","","","",""]# | top>bottom
+ui_func=ui_func+["space",""]# Play/Pause REC
 
 
-func_conf=["none","none","none","none","none","none","none","none"]
-func_conf=func_conf+["none","none","none","none","none","none","none","none"]
-func_conf=func_conf+["none","none","none","none","none","none","none","none"]
-func_conf=func_conf+["none","none","none","none","none","none","none","none"]
-func_conf=func_conf+["w","q","n","none","none","none","none","none"]
 
-ui_but=["40","41","42","43","44","45","46","47","52","53","54","55","56","51","5b","5d"]
-ui_func=["none","none","none","none","none","none","none","none","none","none","none","none","none","none","space","none"]
+
+
+#################################################
 A=list(zip(itertools.repeat("default"),apc_array,color_array,func_conf))#[LED_Mode,Button,colors,functions]
-B=list(zip(ui_func,ui_but))
+B=list(zip(ui_func,ui_but))#[UI_FUNCTIO,UI_BUTTON]
 
+def wayland_xorg(f):
+    if key_func=="wtype":
+        new=[key_func,f]
+        return new
+    else:
+        new=[key_func,"key",f]
+        return new
 
 def init():# A[led,button,color]
     for _ in A:#list(zip(apc_array,itertools.cycle(["light_green","lighter_blue","neon_blue","whiteish","pinker","pinker","red","dunno"]))):
@@ -45,12 +74,15 @@ def change_led(inp):
     if mode=="default":
         subprocess.run(["amidi","--send-hex",str(led_modes["blinking"])+button+color[c],"-p",hw_midi])
         mode="blinking"
-        subprocess.run(["xdotool","key",func])
+        subprocess.run(wayland_xorg(func))
 
     else:
         subprocess.run(["amidi","--send-hex",str(led_modes["default"])+button+color[c],"-p",hw_midi])
         mode="default"
-        subprocess.run(["xdotool","key",func])
+#        new=[key_func,func]
+#        subprocess.run(new)
+ 
+        subprocess.run(wayland_xorg(func))
     return [mode,button,c,func]
        
 init()
@@ -64,7 +96,9 @@ with open("test.log", "w") as f:
             if str(cur).split(" ")[1].lower() in ui_but:
                 cur_input=str(cur).split(" ")[1].lower()
                 B[ui_but.index(cur_input)][0]
-                subprocess.run(["xdotool","key",str(B[ui_but.index(cur_input)][0])])
+
+                subprocess.run(wayland_xorg((str(B[ui_but.index(cur_input)][0]))))
+                
                 subprocess.run(["echo",str(B[ui_but.index(cur_input)])])
 
             else:
@@ -72,13 +106,3 @@ with open("test.log", "w") as f:
 
                 A[apc_array.index(cur_input)]=change_led(cur_input)
                 subprocess.run(["echo",str(A[apc_array.index(cur_input)])])
-
-
-
-
-
-#for y in range(0,127):
-#    print(str(y))
-
-
-#3,4,10,20,24,29,30,34,38,43,48,56,69
